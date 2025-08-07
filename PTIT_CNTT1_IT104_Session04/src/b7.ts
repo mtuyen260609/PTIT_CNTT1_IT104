515a5cc6ec328cca497569e0bfd52d7a21b7f16a
mtuyen260609 <vuminhtuyen26092006@gmail.com>
2025-08-07 23:08:37 +0700

    Update b7.ts

--- PTIT_CNTT1_IT104_Session04/src/b7.ts
+++ PTIT_CNTT1_IT104_Session04/src/b7.ts
@@ -1,26 +1,32 @@
-const processInput = (input: string | number | boolean): string | number => {
-  if (typeof input === "string") {
-    const isNumber = !isNaN(Number(input));
-    if (isNumber) {
-      return Number(input) * Number(input);
-    } else {
-      const letters = input.match(/[a-zA-Z]/g);
-      const count = letters ? letters.length : 0;
-      return `${count} ký tự chữ cái`;
+const processInput = (input: string | number | boolean): void =>{
+    if(typeof input === "string"){
+        if (/^\d+$/.test(input)) {
+            let num = Number(input);
+            console.log(num * num);
+        } else {
+            let count = 0;
+            for (let char of input) {
+                if (/[a-zA-Z]/.test(char)) {
+                    count++;
+                }
+            }
+            console.log(`${count} ký tự chữ cái`);
+        }
+    }else if (typeof input === "number"){
+        let count = 0;
+        for(let i = 1; i <= input; i++){
+            if(input % i === 0) count ++;
+        }
+        if ( count === 2) console.log("Là số nguyên tố")
+        else console.log("KO phải số nguyên tố");
+    }else if (typeof input === "boolean"){
+        if(input === true) console.log("Giá trị là true - tiến hành xử lý")
+        else console.log("Giá trị là false - dừng xử lý");
+    }else {
+        console.log("Định dạng ko hợp lệ");
     }
-  } else if (typeof input === "number") {
-    if (input < 2 || !Number.isInteger(input)) {
-      return "Không phải là số nguyên tố";
-    }
-    for (let i = 2; i <= Math.sqrt(input); i++) {
-      if (input % i === 0) {
-        return "Không phải là số nguyên tố";
-      }
-    }
-    return "Đây là số nguyên tố";
-  } else {
-    return input
-      ? "Giá trị là true - đang tiến hành xử lý"
-      : "Giá trị là false - dừng xử lý";
-  }
-};
+}
+processInput("mot23");
+processInput(123);
+processInput(true);
+processInput(false);
